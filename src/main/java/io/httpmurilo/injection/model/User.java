c80c21f71cf86894aaf427d6cadf352ca0b1b3b0
httpmurilo <muriloeduardo1997@outlook.com>
2024-04-02 21:34:22 -0300

    pontuais

--- src/main/java/io/httpmurilo/injection/model/User.java
+++ src/main/java/io/httpmurilo/injection/model/User.java
@@ -1,15 +1,19 @@
 package io.httpmurilo.injection.model;
 
 
-import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 public class User {
 
     public User() {}
+
+    public User(int id, String name, Status isActive, Status isAdmin) {
+        this.id = id;
+        this.name = name;
+        this.isActive = isActive;
+        this.isAdmin = isAdmin;
+    }
 
     @Id
     private int id;
@@ -18,4 +22,36 @@ public class User {
     private Status isActive;
     @Enumerated(EnumType.STRING)
     private Status isAdmin;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Status getIsActive() {
+        return isActive;
+    }
+
+    public void setIsActive(Status isActive) {
+        this.isActive = isActive;
+    }
+
+    public Status getIsAdmin() {
+        return isAdmin;
+    }
+
+    public void setIsAdmin(Status isAdmin) {
+        this.isAdmin = isAdmin;
+    }
 }
--- src/main/resources/data.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO "user" (id, name, isactive, isadmin) VALUES (1, "Artur", "TRUE", "TRUE");
-INSERT INTO "user" (id, name, isactive, isadmin) VALUES (2, "Maria", "TRUE", "FALSE");
-INSERT INTO "user" (id, name, isactive, isadmin) VALUES (3, "Pedro", "FALSE", "TRUE");
-INSERT INTO "user" (id, name, isactive, isadmin) VALUES (4, "João", "FALSE", "FALSE");
-INSERT INTO "user" (id, name, isactive, isadmin) VALUES (5, "Gui", "FALSE", "TRUE");
-INSERT INTO "user" (id, name, isactive, isadmin) VALUES (6, "Henrique", "TRUE", "FALSE");
-INSERT INTO "user" (id, name, isactive, isadmin) VALUES (7, "Mateus", "FALSE", "TRUE");
-INSERT INTO "user" (id, name, isactive, isadmin) VALUES (8, "Marcilio", "FALSE", "TRUE");
